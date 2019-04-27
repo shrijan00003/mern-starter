212f8e38e0b00cd426df75304d233c87a8bc00e2
shrijan <shrijan00003@gmail.com>
2019-04-27 23:25:21 +0545

    day-10-profile-routes: profile get route is added end of 18

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//import profile and user model
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
 
 /**
  * @route GET api/profile/test
@@ -11,5 +17,27 @@ router.get("/test", (req, res) => {
     msg: "proflie works"
   });
 });
+
+/**
+ * @route GET api/profile/
+ * @description Get Current user Profile
+ * @access Private
+ */
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json({ err }));
+  }
+);
 
 module.exports = router;
