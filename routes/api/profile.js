6b6870fe0ac7ebb90324bbe9a8ab285bc7df5328
shrijan <shrijan00003@gmail.com>
2019-04-28 13:37:03 +0545

    day-10-profile-routes: basic profile create and update route is added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -39,5 +39,77 @@ router.get(
       .catch(err => res.status(404).json({ err }));
   }
 );
+/**
+ * @route POST api/profile/
+ * @description create or update the user profile
+ * @access Private
+ */
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    //Get fields
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubusername)
+      profileFields.githubusername = req.body.githubusername;
+    //splits skills into array
+    if (typeof req.body.skills !== undefined) {
+      profileFields.skills = req.body.skills.split(",");
+    }
+    //social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (!profile) {
+        //update will happen here
+        Profile.findOneAndDelete(
+          { user: req.user.id },
+          {
+            $set: profileFields
+          },
+          {
+            new: true
+          }
+        )
+          .then(updateProfile => res.json(updateProfile))
+          .catch(err => {
+            errros.msg = "Error in updating profile";
+            errors.error = err;
+            res.status(400).json(errors);
+          });
+      } else {
+        //create
+        //check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "That handle already exists";
+            res.status(400).json(errors);
+          }
+          //save profile
+          new Profile(profileFields)
+            .save()
+            .then(newProfile => req.json(newProfile))
+            .catch(err => {
+              errros.msg = `Error in creating profile`;
+              errors.error = err;
+              res.status(400).json(errors);
+            });
+        });
+      }
+    });
+  }
+);
 
 module.exports = router;
